871b61a75e7b26a248c40740fd75636f0f5c688a
S.A.A Perera <it20246914@my.sliit.lk>
2023-11-13 20:18:27 +0530

    Implemented role based login

--- src/main/java/com/avinash/danumalk/DanumaLkApplication.java
+++ src/main/java/com/avinash/danumalk/DanumaLkApplication.java
@@ -1,8 +1,14 @@
 package com.avinash.danumalk;
 
+import com.avinash.danumalk.auth.AuthenticationService;
+import com.avinash.danumalk.auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+
+import static com.avinash.danumalk.user.Role.ADMIN;
+import static com.avinash.danumalk.user.Role.MANAGER;
 
 @SpringBootApplication(scanBasePackages = "com.avinash.danumalk")
 public class DanumaLkApplication {
@@ -10,5 +16,30 @@ public class DanumaLkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DanumaLkApplication.class, args);
 	}
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
 
+			var manager = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+		};
+	}
 }
+
